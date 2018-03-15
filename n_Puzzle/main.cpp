8031749b09ab4117ecb6c3ef05823b0bd31a768f
reydeleo <reynaldo_car@live.com>
2018-03-15 18:50:38 -0500

    Initial Commit

---
+++ n_Puzzle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  n_Puzzle
+//
+//  Created by reynaldo deleo on 3/15/18.
+//  Copyright © 2018 reynaldo deleo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
